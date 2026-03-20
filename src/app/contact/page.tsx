a231fe028363357778eb36645db67a1125ce4d5e
Areeb Malik <areeb9273@gmail.com>
2026-03-20 20:06:18 +0500

    Updated with boiler plate for specific this branch

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 import ContactScreen from "@/screens/Contact/ContactScreen";
 
 export const metadata: Metadata = {
-  title: "Contact",
+  title: "Contact",     //contact
   description: "Get in touch — I'm always open to new opportunities and collaborations.",
 };
 
